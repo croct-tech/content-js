8cbfe9d1574107cf495c562a80111a680de96eaf
Marcos Passos <marcos@croct.com>
2025-03-16 16:09:51 -0300

    Add side effects false

---
+++ src/slot/getSlotContent.ts
@@ -0,0 +1,5 @@
+import type {JsonObject} from '@croct/json';
+
+export function getSlotContent(slotId: string, language?: string): JsonObject|null {
+    return null;
+}
---
+++ src/slot/loadSlotContent.ts
@@ -0,0 +1,5 @@
+import type {JsonObject} from '@croct/json';
+
+export function loadSlotContent(slotId: string, language?: string): Promise<JsonObject|null> {
+    return Promise.resolve(null);
+}
