7064e901ac6052f6c802e1d9387c00c89f1119dd
Marcos Passos <marcos@croct.com>
2025-03-16 16:11:19 -0300

    Fix tests

--- test/slot/index.test.ts
+++
@@ -1,7 +0,0 @@
-import {getSlotContent} from '../../src';
-
-describe('getSlotContent', () => {
-    it('should return null', async () => {
-        await expect(getSlotContent('bar', 'en')).resolves.toBeNull();
-    });
-});
